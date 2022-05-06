bcbc313571e9d1fc8f40a3496328944d3dd4029a
nitin.raghuwanshi <nitin.raghuwanshi@olaelectric.com>
2022-05-06 16:47:18 +0530

    Made initialiser public for LoggerForm

--- Sources/OlaLogger/LoggerForm.swift
+++ Sources/OlaLogger/LoggerForm.swift
@@ -7,18 +7,22 @@
 
 import Foundation
 
-protocol LoggerFormType {
+public protocol LoggerFormType {
     var id: String {get set}
     var fields: [String: String] {get set}
 }
-extension LoggerFormType {
+public extension LoggerFormType {
     var fieldsParamString: String {
         return fields.responseSubmissionString
     }
 }
 public struct LoggerForm: LoggerFormType {
-   public var id: String
-   public var fields: [String : String]
+    public var id: String
+    public var fields: [String : String]
     
+    public init(id: String, fields: [String : String]) {
+        self.id = id
+        self.fields = fields
+    }
 }
 
